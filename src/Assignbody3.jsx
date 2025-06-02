b5802ce6e54b448ad956c886224acf9ebc7d3bc1
Chinexagu <Chinexagu10@gmail.com>
2025-06-02 04:15:50 -0700

    hii

--- src/Assignbody3.jsx
+++ src/Assignbody3.jsx
@@ -9,7 +9,7 @@ function Assignbody3() {
             <img src="workplace edited.jpg" alt="working" className=' absolute top-[27rem] left-[-5rem] w-[15rem] h-[20rem]' />
             <img src="working edited.jpg" alt="computers" className=' absolute w-[15rem] top-[5rem] right-[-7rem] h-[15rem]' />
             <div className=' w-[35rem] h-[25rem] bg-black absolute bottom-[13rem] right-[-27rem] text-white text-[3rem] font-bold pt-[1rem] text-left pl-[2rem]'>
-                Our business consultants can help you adapt to today’s market dynamics hioi
+                Our business consultants can help you adapt to today’s market dynamics nope
             </div>
         </div>
         <Assignbody4 />
